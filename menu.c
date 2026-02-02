0a5092e78ce128e1c7770741bd0f0b36833e3a96
oschmick <oschmick@pugetsound.edu>
2026-02-02 02:18:30 +0000

    Put menu options in their own printf lines

--- menu.c
+++ menu.c
@@ -8,8 +8,8 @@
 // TODO - menu functions
 void printMenu() {
     printf("*** WORD STATS MENU ***");
-    printf("Enter 1 to print frequencies.\n
-            Enter 2 to print histogram.\n
-            Enter 3 to return to inputting more strings.\n
-            Enter 4 to quit.\n");
+    printf("Enter 1 to print frequencies.\n");
+    printf("Enter 2 to print histogram.\n");
+    printf("Enter 3 to return to inputting more strings.\n");
+    printf("Enter 4 to quit.\n");
 }
